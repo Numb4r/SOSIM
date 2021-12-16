b3bd419a9de9a41174d41113a24d19978cf07b92
Numb4r <yuridimitre@protonmail.com>
2021-12-16 18:02:42 -0300

    criado arquivo para leitura de json

---
+++ core/FileIO/fileIO.hpp
@@ -0,0 +1,20 @@
+#include "../../modules/json/json.hpp"
+#include <fstream>
+#include <sstream>
+using json = nlohmann::json;
+class FileIO{
+    public:
+    static json readJson(const char* filePath){
+        std::ifstream file ;
+        file.open(filePath);
+        if(!file.is_open())
+            throw "erro ao abrir o arquivo";
+        std::string line;
+        std::stringstream fileBody;
+        while(std::getline(file,line))
+            fileBody << line;
+        file.close();
+        return json::parse(fileBody);
+    }
+
+};
