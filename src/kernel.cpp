8fe08371e36a092f78cdaa6f55c5ed1180cab1cb
Numb4r <yuridimitre@protonmail.com>
2021-12-16 21:45:24 -0300

    implementacao do kernel inicia os hardwares

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -1,15 +1,14 @@
 #include "kernel.hpp"
 #include "json.hpp"
-Kernel::Kernel(const char* fileProcessPath){
+Kernel::Kernel(const char* fileProcessPath):cpu(CPU()),ram(RAM()),disk(Disk()){
     
     nlohmann::json j = Bootloader().boot(fileProcessPath);
-    std::string  pc= j["processCount"];
-    this->escalonador = Escalonador(atoi(pc.c_str()));
+    const int pc= j["processCount"];
+    this->escalonador = Escalonador(pc);
+    printf("%d",this->escalonador.getProcessCountMax());
     for (auto&&i : j["process"]) {
         //escalonador.addProcessToList(i);
     }
+    this->shell = Shell(&cpu,&ram,&disk);
 }
-Kernel::Kernel(){
-    this->escalonador = Escalonador(1);
-    printf("%d",escalonador.getProcessCountMax());
-}
+
