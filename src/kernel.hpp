d8802611bce30390ef4dac56701696d8c0661899
Numb4r <yuridimitre@protonmail.com>
2021-12-16 21:44:50 -0300

    adicionado shell ao kernel

--- src/kernel.hpp
+++ src/kernel.hpp
@@ -4,14 +4,14 @@
 #include "cpu.hpp"
 #include "ram.hpp"
 #include "disk.hpp"
-
+#include "shell.hpp"
 class Kernel{
         CPU cpu;
         RAM ram;
         Disk disk;
         Escalonador escalonador;
+        Shell shell;
     public:
         Kernel(const char* fileProcessPath);
-        Kernel();
 
 };
