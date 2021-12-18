ed82fdd38944791bf46f6de10bd8490c1502ad36
Numb4r <yuridimitre@protonmail.com>
2021-12-18 16:58:41 -0300

    criado fila de recursos a ser  pelo processo

--- src/process.cpp
+++ src/process.cpp
@@ -1,26 +1,19 @@
 #include "process.hpp"
 
+Process::Process(int cycles, std::queue<resources> queueResources)
+    : cycles(cycles), queueResources(queueResources) {}
+void Process::changeState(enum ::states state) { this->state = state; }
+void Process::changePriority(const enum ::priorities priority) {
+  this->priority = priority;
+}
 
-Process::Process(const int pid):pid(pid),priority(medium),timestamp(0),quantum(0),state(criado){}
-Process::Process(const int pid,const enum::priorities priority):pid(pid),priority(priority),timestamp(0),quantum(0),state(criado){}
-void Process::changeState(enum ::states state){
-    this->state = state;
-}
-void Process::changePriority(const enum ::priorities priority){
-    this->priority = priority;
-}
-void Process::setQuantum(const int quantum){
-    this->quantum = quantum;
-}
-void Process::addTimestamp(const int add){
-    this->timestamp+= add;
-}
-int Process::getPID() const{
-    return this->pid;
-}
-int Process::getPriority() const{
-    return this->priority;
-}
-int Process::getTimestamp() const{
-    return this->timestamp;
-}
+void Process::makeCycle() { this->timestamp++; }
+
+void Process::setQuantum(const int quantum) { this->quantum = quantum; }
+
+int Process::getPID() const { return this->pid; }
+int Process::getQuantum() const { this->quantum; }
+int Process::getPriority() const { return this->priority; }
+int Process::getTimestamp() const { return this->timestamp; }
+int Process::getCycles() const { this->cycles; }
+bool Process::isTerminated() const { return timestamp >= cycles; }
--- src/process.hpp
+++ src/process.hpp
@@ -1,33 +1,30 @@
 #pragma once
-enum states{
-    criado ,
-    pronto,
-    execucao,
-    bloqueado,
-    finalizado
-};
-enum priorities{
-    realtime,
-    high,
-    medium,
-    low,
-    zero
-};
-class Process{
-    int pid;
-    int timestamp ;
-    int quantum;
-    enum::priorities priority;
-    enum::states state;
-    public:
-        Process(const int pid);
-        Process(const int pid,const enum::priorities priority);
-        
-        void changeState(enum::states state);
-        void changePriority(const enum::priorities priority);
-        void setQuantum(const int quantum);
-        void addTimestamp(const int add);
-        int getPID() const;
-        int getPriority()const;
-        int getTimestamp()const;
+#include <queue>
+enum states { criado, pronto, execucao, bloqueado, finalizado };
+enum priorities { realtime, high, medium, low, zero };
+enum resources { cpu, ram, disk };
+class Process {
+  int pid;
+  int timestamp;
+  int quantum;
+  int cycles;
+  std::queue<resources> queueResources;
+  enum ::priorities priority;
+  enum ::states state;
+
+public:
+  Process(int cycles, std::queue<resources> queueResources);
+
+  void setPID(const int pid);
+  void changeState(enum ::states state);
+  void changePriority(const enum ::priorities priority);
+  void setQuantum(const int quantum);
+  void makeCycle();
+
+  int getPID() const;
+  int getPriority() const;
+  int getTimestamp() const;
+  int getQuantum() const;
+  int getCycles() const;
+  bool isTerminated() const;
 };
