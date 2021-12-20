f7c3e6e4b6e5dbf03d8c83c4936d15da48b3d2dc
Numb4r <yuridimitre@protonmail.com>
2021-12-19 21:03:28 -0300

    corrigido metodos gets nao retornando

--- src/process.cpp
+++ src/process.cpp
@@ -1,7 +1,6 @@
 #include "process.hpp"
-
 Process::Process(int cycles, std::queue<resources> queueResources)
-    : cycles(cycles), queueResources(queueResources) {}
+    : cycles(cycles), queueResources(queueResources), timestamp(0) {}
 void Process::changeState(enum ::states state) { this->state = state; }
 void Process::changePriority(const enum ::priorities priority) {
   this->priority = priority;
@@ -10,10 +9,10 @@ void Process::changePriority(const enum ::priorities priority) {
 void Process::makeCycle() { this->timestamp++; }
 
 void Process::setQuantum(const int quantum) { this->quantum = quantum; }
-
+void Process::setPID(const int pid) { this->pid = pid; }
 int Process::getPID() const { return this->pid; }
-int Process::getQuantum() const { this->quantum; }
+int Process::getQuantum() const { return this->quantum; }
 int Process::getPriority() const { return this->priority; }
 int Process::getTimestamp() const { return this->timestamp; }
-int Process::getCycles() const { this->cycles; }
+int Process::getCycles() const { return this->cycles; }
 bool Process::isTerminated() const { return timestamp >= cycles; }
--- src/process.hpp
+++ src/process.hpp
@@ -14,6 +14,7 @@ class Process {
 
 public:
   Process(int cycles, std::queue<resources> queueResources);
+  Process() = default;
 
   void setPID(const int pid);
   void changeState(enum ::states state);
