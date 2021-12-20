5fdddef339bc34ec5f5ae5d12c32514c7e0a565f
Numb4r <yuridimitre@protonmail.com>
2021-12-19 21:04:34 -0300

    criado  execucao do sistema por parte do kernel

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -1,13 +1,37 @@
 #include "kernel.hpp"
 #include "json.hpp"
-Kernel::Kernel(const char* fileProcessPath):cpu(CPU()),ram(RAM(RAMMAX)),disk(Disk()){
-    
-    nlohmann::json j = Bootloader().boot(fileProcessPath);
-    const int pc= j["processCount"];
-    this->escalonador = Escalonador(pc);
-    printf("%d",this->escalonador.getProcessCountMax());
-    for (auto&&i : j["process"]) {
-        //escalonador.addProcessToList(i);
-    }
-}
+#include "process.hpp"
+Kernel::Kernel(const char *fileProcessPath)
+    : cpu(CPU(NUMCORES)), ram(RAM(RAMMAX)), disk(Disk()), isRunning(false) {
 
+  nlohmann::json j = Bootloader().boot(fileProcessPath);
+  const int pc = j["processCount"];
+  const int it = j["interval"];
+  this->escalonador = Escalonador(pc, it);
+  for (auto &&i : j["process"]) {
+    escalonador.addProcessToList(i);
+  }
+}
+void Kernel::executeSystem() {
+  this->isRunning = true;
+  int cycles = 0;
+  while (this->isRunning) {
+    this->escalonador.createProcess(cycles);
+
+    this->ps = this->escalonador.getNextProcess(cycles);
+    if (!ps) {
+      // ps->makeCycle();
+      this->isRunning = false;
+      break;
+    }
+    printf("[%d]:", cycles);
+    ps->makeCycle();
+    printf("%d q:%d ts:%d cycles:%d\n", ps->getPID(), ps->getQuantum(),
+           ps->getTimestamp(), ps->getCycles());
+    cycles++;
+  }
+  // for (auto &&i : this->escalonador.deadProcess) {
+  //   printf("\n%d %d %d %d", i.getPID(), i.isTerminated(), i.getQuantum(),
+  //          i.getTimestamp());
+  // }
+}
--- src/kernel.hpp
+++ src/kernel.hpp
@@ -1,18 +1,28 @@
 #pragma once
+#include <array>
 #define NUMCORES 1
 #define RAMMAX 5
 #define DISKSPACE 10
 #include "bootloader.hpp"
-#include "escalonador.hpp"
 #include "cpu.hpp"
-#include "ram.hpp"
 #include "disk.hpp"
-class Kernel{
-        CPU cpu;
-        RAM ram;
-        Disk disk;
-        Escalonador escalonador;
-    public:
-        Kernel(const char* fileProcessPath);
+#include "escalonador.hpp"
+#include "process.hpp"
+#include "ram.hpp"
+#include <array>
+// Acho que ja to ficando doidao
+// Nada mais faz sentido
+// Alguem me salva
+class Kernel {
+  CPU cpu;
+  RAM ram;
+  Disk disk;
+  Escalonador escalonador;
+  bool isRunning;
+  Process *ps;
 
+public:
+  Kernel(const char *fileProcessPath);
+  void executeSystem();
+  void stopSystem();
 };
