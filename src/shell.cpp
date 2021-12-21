652b554781f3db181befa6be8d2367dd158f09a3
Numb4r <yuridimitre@protonmail.com>
2021-12-21 18:31:19 -0300

    criada funcoes para mostrar as informacoes da cpu e ram

--- src/shell.cpp
+++ src/shell.cpp
@@ -1,33 +1,89 @@
 #include "shell.hpp"
 #include "json.hpp"
+#include <algorithm>
 #include <functional>
 #include <iostream>
 #include <stdio.h>
 #include <string>
+#include <vector>
 Shell::Shell(Kernel *kernel) : kernel(kernel) {}
 
 void Shell::help() {
-  printf("help:\tDemonstra quais comandos podem ser executados pelo shell "
-         "desenvolvido\n");
-  printf("meminfo:\tDetalha quantas unidades de memória so sistema possui, "
-         "quantas estão livres e quantas estão alocadas e para quais "
-         "processos.\n");
-  printf("cpuinfo:\tDetalha qual processo está em execução, qual o timestamp e "
-         "quantum desse.\n");
+  printf("help:\tMostra comandos do shell\n");
+  printf("meminfo:\tInformacoes sobre a memoria RAM.\n");
+  printf("cpuinfo:\tInformacoes sobre o CPU.\n");
   printf("queueschell:\tDetalha quais processos estão sendo gerenciados pelo "
-         "seu sistema, quais estão em estado de pronto, bloqueado, execução "
-         "e/ou sendo criados e finalizados.\n");
-  printf("execute:\tExecuta a fila de processos definida conforme configuração "
-         "prévia.\n");
-  printf("kill -9:\tFinaliza a execução do sistema operacional, voltando o "
-         "mesmo para o estado inicial, em que os processos estariam ainda em "
-         "faze de criação inicial.\n");
+         "sistema\n");
+  printf("execute:\tExecuta a fila de processos definida\n");
+  printf("kill -9:\tFinaliza a execução do sistema operacional\n");
+  printf("membench:\tExecuta um full load na RAM para fins de teste\n");
+  printf("cpubench:\tExecuta um full load no CPU para fins de teste\n");
 }
-void Shell::meminfo() { nlohmann::json j = json::parse(kernel->ssMemory()); }
-void Shell::cpuinfo() { nlohmann::json j = json::parse(kernel->ssCPU()); }
+void Shell::meminfo() {
+  nlohmann::json j = json::parse(kernel->ssMemory());
+  int memSize = j["memorySize"];
+  auto vec = j["process"];
+
+  printf("[");
+  std::vector<int> address;
+  for (auto &&i : vec) {
+    address.push_back(i["adr"]);
+  }
+
+  for (int i = 0; i < memSize; i++) {
+    if (std::find(address.begin(), address.end(), i) != address.end())
+      printf("///////");
+    else
+      printf("\t");
+    if (i + 1 < memSize)
+      printf("|");
+  }
+  std::string s;
+  printf("]\n");
+  std::vector<std::string> info = {"pid", "adr", "pr", "ts", "qt"};
+  printf("{");
+  for (auto &&i : info) {
+    printf("\n%s : ", i.c_str());
+    for (auto &&j : vec) {
+      std::cout << j[i] << "\t";
+    }
+  }
+  printf("\n}\n");
+}
+void Shell::cpuinfo() {
+  nlohmann::json j = json::parse(kernel->ssCPU());
+
+  int cores = j["numCores"];
+  auto vec = j["process"];
+  std::vector<int> cr;
+  for (auto &&i : vec) {
+    cr.push_back(i["cr"]);
+  }
+  for (int i = 0; i < cores; i++) {
+    if (std::find(cr.begin(), cr.end(), i) != cr.end())
+      printf("[///////////////////////]");
+    else
+      printf("[\t\t\t]");
+  }
+  std::string s;
+
+  std::vector<std::string> info = {"pid", "pr", "ts", "qt"};
+  printf("\n{");
+  for (auto &&i : info) {
+    printf("\n%s : ", i.c_str());
+    for (auto &&j : vec) {
+      std::cout << j[i] << "\t";
+    }
+  }
+  printf("\n}\n");
+}
 void Shell::queueschell() {}
 void Shell::execute() { this->kernel->executeSystem(); }
-void Shell::kill() { this->kernel->reboot(); }
+void Shell::cpubench() { this->kernel->benchmarkCPU(); }
+void Shell::membench() { this->kernel->benchmarkRAM(); }
+void Shell::kill() {
+  // this->kernel->reboot();
+}
 void Shell::loop() {
   while (true) {
 
@@ -46,6 +102,10 @@ void Shell::loop() {
       execute();
     } else if (std::strcmp(readline, "kill -9") == 0) {
       kill();
+    } else if (std::strcmp(readline, "cpubench") == 0) {
+      this->cpubench();
+    } else if (std::strcmp(readline, "membench") == 0) {
+      this->membench();
     } else {
       printf("bash: %s : Comando nao encontrado\n", readline);
     }
--- src/shell.hpp
+++ src/shell.hpp
@@ -2,22 +2,15 @@
 #include "kernel.hpp"
 class Shell {
   Kernel *kernel;
-  void help(); // help: Demonstra quais comandos podem ser executados pelo shell
-               // desenvolvido;
-  void meminfo(); // meminfo: detalha quantas unidades de memória so sistema
-                  // possui, quantas estão livres e quantas estão alocadas e
-                  // para quais processos.
-  void cpuinfo(); // cpuinfo: detalha qual processo está em execução, qual o
-                  // timestamp e quantum desse.
-  void
-  queueschell(); // queueschell: detalha quais processos estão sendo gerenciados
-                 // pelo seu sistema, quais estão em estado de pronto,
-                 // bloqueado, execução e/ou sendo criados e finalizados.
-  void execute(); // execute: Executa a fila de processos definida conforme
-                  // configuração prévia.
-  void kill(); // kill -9: Finaliza a execução do sistema operacional, voltando
-               // o mesmo para o estado inicial, em que os processos estariam
-               // ainda em faze de criação inicial.
+  void help();
+  void meminfo();
+  void cpuinfo();
+  void queueschell();
+  void execute();
+  void kill();
+  void membench();
+  void cpubench();
+
 public:
   Shell(Kernel *kernel);
   void loop();
