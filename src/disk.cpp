5ff374bf02cab94ab88be30f3fef4f25df4a30f2
Numb4r <yuridimitre@protonmail.com>
2021-12-21 19:01:07 -0300

    criado disco

--- src/disk.cpp
+++ src/disk.cpp
@@ -1,2 +1,8 @@
 #include "disk.hpp"
 void Disk::reset() {}
+void Disk::createDir(const char *name) {
+  this->path.push_back(nodePath(name, true));
+}
+void Disk::createFile(const char *name) {
+  this->path.push_back(nodePath(name, false));
+}
--- src/disk.hpp
+++ src/disk.hpp
@@ -1,5 +1,21 @@
 #pragma once
+#include <cstddef>
+#include <list>
+#include <string>
+#include <vector>
+class nodePath {
+  bool isDirectory;
+  std::string name;
+
+public:
+  nodePath(std::string name, bool isDirectory)
+      : name(name), isDirectory(isDirectory) {}
+};
 class Disk {
+  std::vector<nodePath> path;
+
 public:
   void reset();
+  void createDir(const char *name);
+  void createFile(const char *name);
 };
