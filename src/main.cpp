28e5fde371fa1d8062ff33c9fdaffeff0f824211
Numb4r <yuridimitre@protonmail.com>
2021-12-21 19:09:05 -0300

    criado main definitivo

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,8 @@
 #include "kernel.hpp"
 #include "shell.hpp"
-
-int main(const int argc,char** argv ){
-    Kernel kernel(argv[1]);
-    Shell shell(&kernel);
-    shell.input();
-   
-
-    return 0;
+int main(const int argc, char **argv) {
+  Kernel kernel("hardware.json", "processos.json");
+  Shell shell(&kernel);
+  shell.loop();
+  return 0;
 }
