5d9095cb583c1be6cb2098e046971299352c47ff
Numb4r <yuridimitre@protonmail.com>
2022-01-17 19:18:38 -0300

    mudanca no comportamento do makecycle// agora n e responsabilidad do esc excluir ps

--- src/escalonador.cpp
+++ src/escalonador.cpp
@@ -10,18 +10,9 @@ Escalonador::Escalonador(enum schedulerPolicy policy) : policy(policy) {
 }
 enum schedulerPolicy Escalonador::getPolicy() const { return this->policy; }
 void Escalonador::applyPolicy(std::list<Process> &listOfProcess) {
-  printf("Applying policy: ");
+  printf("Applying policy:\n");
   std::vector<Process> arrayP{std::make_move_iterator(listOfProcess.begin()),
                               std::make_move_iterator(listOfProcess.end())};
-  for (int i = 0; i < arrayP.size(); i++) {
-    if (arrayP.at(i).isProcessTerminated()) {
-      // Escrever logica para pegar o log aqui quando o processo finalizar?
-      //  Move ele para um lista de terminatedProcess?
-      printf("erasing %d", arrayP.at(i).getPID());
-      arrayP.erase(arrayP.begin() + i);
-    }
-  }
-
   switch (this->policy) {
   case schedulerPolicy::FIFO:
     listOfProcess = this->FIFO(arrayP);
@@ -31,8 +22,9 @@ void Escalonador::applyPolicy(std::list<Process> &listOfProcess) {
 // TODO: Consumir timestamp. Verificar se o uso do Quantum esta de forma correta
 // TODO: Criar classe/funcao de Log para fazer output em um json
 void Escalonador::makeCycle(std::list<Process> &listProcess) {
-  printf("Making cycle %d\n", listProcess.empty());
+  printf("Making cycle\n");
   int seconds{};
+  int timestamp{};
   for (Process &ps : listProcess) {
     if (!ps.isProcessTerminated()) {
       printf("Consumindo ");
@@ -45,17 +37,25 @@ void Escalonador::makeCycle(std::list<Process> &listProcess) {
                seconds);
         std::this_thread::sleep_for(std::chrono::seconds(seconds));
         ps.changeState(states::pronto);
+        timestamp += seconds;
       } else {
         printf("cpu\n");
       }
       ps.changeState(states::execucao);
       seconds = (rand() % ps.getMaxQuantum()) + 1;
-      printf("PID:%d\t Seconds:%d\t ciclosRestantes: %d\n ", ps.getPID(),
-             seconds, ps.getCycles());
+      if (ps.getQuantum() - seconds < 0) {
+        seconds = ps.getQuantum();
+      }
+      timestamp += seconds;
+      printf(
+          "PID:%d\t Seconds:%d maxQuantum:%d quantumAtual:%d\t timestamp:%d\t  "
+          "ciclosRestantes: %d\n ",
+          ps.getPID(), seconds, ps.getMaxQuantum(), ps.getQuantum(), timestamp,
+          ps.getCycles());
       std::this_thread::sleep_for(std::chrono::seconds(seconds));
       ps.changeState(states::pronto);
       ps.changeResourceConsumed((rand() % 3));
-      ps.makeCycle();
+      ps.makeCycle(timestamp, seconds);
     }
   }
 }
